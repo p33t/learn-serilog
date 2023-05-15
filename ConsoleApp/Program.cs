aaa24bbd5d1727cc656549f43dd59d720aeb6e1b
p33t <1179301+p33t@users.noreply.github.com>
2023-05-15 15:04:47 -0700

    Logging to multiple manually configured and JSON configured instances

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,10 +1,47 @@
-﻿using Serilog;
+﻿using Microsoft.Extensions.Configuration;
+using Serilog;
+using Serilog.Settings.Configuration;
 
+// Using multiple logging configurations ###########################################################
 using var logger = new LoggerConfiguration()
     .MinimumLevel.Debug()
     .WriteTo.Console()
-    .WriteTo.File("logs/myapp.txt", rollingInterval: RollingInterval.Day)
+    .WriteTo.File("logs/primary.txt", rollingInterval: RollingInterval.Day)
+    .CreateLogger();
+
+using var altLogger = new LoggerConfiguration()
+    .MinimumLevel.Debug()
+    .WriteTo.Console()
+    .WriteTo.File("logs/alternate.txt", rollingInterval: RollingInterval.Day)
+    .CreateLogger();
+
+logger.Information("Information message with {param}", "some-param-value");
+altLogger.Information("Information message with {param}", "some-alt-param-value");
+
+
+// Using a JSON file configuration ################################################################
+IConfiguration configuration = new ConfigurationBuilder()
+    .AddJsonFile("config.json") //, optional: true, reloadOnChange: true)
+    .Build();
+
+
+var minimumLevel = configuration.GetSection("Serilog").GetValue<string>("MinimumLevel");
+logger.Information("MinimumLevel from config is {0}", minimumLevel);
+
+using var configLogger = new LoggerConfiguration()
+    .ReadFrom
+    .Configuration(configuration)
+    // .Configuration(configuration.GetSection("Serilog"))
+    .CreateLogger();
+
+configLogger.Warning("Warning message with {param}", "some-config-param-value");
+
+// The alternate configuration has a different minimum level
+using var altConfigLogger = new LoggerConfiguration()
+    .ReadFrom
+    .Configuration(configuration, new ConfigurationReaderOptions { SectionName = "AltSerilog" })
+    // .Configuration(configuration.GetSection("Serilog"))
     .CreateLogger();
     
-logger.Information("Information message with {param}", "some-param-value");
+altConfigLogger.Verbose("Verbose message to alt config logger");
     
